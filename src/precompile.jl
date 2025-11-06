c62c9585057d08f81c3b43cd1e03393c9072cb2a
Hantao Cui <cuihantao@gmail.com>
2025-11-05 22:35:57 -0500

    Add precompilation workload for NPCC dyr file parsing; implement common accessor patterns and fallback parsing mode

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,10 +1,35 @@
 # Precompilation workload
 
 @setup_workload begin
-    # Put setup code here, to be executed before precompiling
+    # Get path to test NPCC dyr file for precompilation
+    npcc_dyr_path = joinpath(pkgdir(PowerDynData), "test", "testfiles", "npcc", "npcc_full.dyr")
+    metadata_path = joinpath(pkgdir(PowerDynData), "metadata")
 
     @compile_workload begin
-        # Precompile common operations
-        # TODO: Add precompilation workload once parsing is implemented
+        # Precompile parsing operations using NPCC test file
+        if isfile(npcc_dyr_path)
+            # Parse with metadata (most common usage pattern)
+            dd = parse_dyr(npcc_dyr_path, metadata_dir=metadata_path)
+
+            # Precompile common accessor patterns
+            if !isempty(dd.models)
+                # Get first model type
+                first_model = first(values(dd.models))
+
+                # Access model data (precompiles getindex operations)
+                if haskey(dd.models, "GENROU")
+                    genrou = dd["GENROU"]
+                end
+
+                # Precompile iteration
+                for (name, model) in dd.models
+                    # Common access pattern
+                    break  # Just one iteration for precompilation
+                end
+            end
+
+            # Parse without metadata (fallback mode)
+            dd_indexed = parse_dyr(npcc_dyr_path, metadata_dir=nothing)
+        end
     end
 end
